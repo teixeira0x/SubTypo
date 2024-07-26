235b2153ceebc52240345d14165077ef7d60fcd2
teixeira0x <secretedit123e@gmail.com>
2024-07-26 19:37:38 -0300

    add simple subtitle timedtext source generator

--- app/src/main/java/com/teixeira/subtitles/subtitlefile/SubtitleSourceMaker.java
+++ app/src/main/java/com/teixeira/subtitles/subtitlefile/SubtitleSourceMaker.java
@@ -1,6 +1,7 @@
 package com.teixeira.subtitles.subtitlefile;
 
 import com.teixeira.subtitles.models.Subtitle;
+import com.teixeira.subtitles.utils.VideoUtils;
 import java.util.List;
 
 public class SubtitleSourceMaker {
@@ -20,6 +21,37 @@ public class SubtitleSourceMaker {
       sb.append(subtitle.getText());
       sb.append("\n\n");
     }
+
+    return sb.toString().trim();
+  }
+
+  public String makeTimedTextSource(List<Subtitle> subtitles) {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n");
+    sb.append("<timedtext format=\"3\">\n");
+    sb.append("<body>\n");
+
+    for (int i = 0; i < subtitles.size(); i++) {
+
+      Subtitle subtitle = subtitles.get(i);
+
+      long startTime = VideoUtils.getMilliSeconds(subtitle.getStartTime());
+      long endTime = VideoUtils.getMilliSeconds(subtitle.getEndTime());
+
+      sb.append(
+          "<p t=\""
+              + startTime
+              + "\" d=\""
+              + (endTime - startTime)
+              + "\">"
+              + subtitle.getText()
+              + "</p>");
+      sb.append("\n");
+    }
+
+    sb.append("</body>\n");
+    sb.append("</timedtext>");
 
     return sb.toString().trim();
   }

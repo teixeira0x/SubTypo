e73710050b4d7bfaf3d1938bb1bcc8e82b30b745
teixeira0x <secretedit123e@gmail.com>
2024-08-07 16:35:26 -0300

    add crash activity

--- app/src/main/java/com/teixeira/subtitles/App.java
+++ app/src/main/java/com/teixeira/subtitles/App.java
@@ -6,17 +6,25 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceManager;
+import com.blankj.utilcode.util.ThrowableUtils;
 import com.google.android.material.color.DynamicColors;
+import com.teixeira.subtitles.activities.CrashActivity;
 import com.teixeira.subtitles.preferences.Preferences;
 
 public class App extends Application {
+  
+  public static final String APP_REPO_URL = "https://github.com/teixeira0x/SubTypo";
+  public static final String APP_REPO_OPEN_ISSUE = APP_REPO_URL + "/issues/new";
 
   private static App sInstance;
 
+  private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
   private SharedPreferences defaultPrefs;
 
   @Override
   public void onCreate() {
+    uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+    Thread.setDefaultUncaughtExceptionHandler(this::uncaughtException);
     sInstance = this;
     super.onCreate();
 
@@ -44,5 +52,22 @@ public class App extends Application {
 
   public static App getInstance() {
     return sInstance;
+  }
+
+  public void uncaughtException(Thread thread, Throwable throwable) {
+    try {
+      // Start the crash activity
+      Intent intent = new Intent(this, CrashActivity.class);
+      intent.putExtra(CrashActivity.KEY_ERROR, ThrowableUtils.getFullStackTrace(throwable));
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      startActivity(intent);
+
+      if (uncaughtExceptionHandler != null) {
+        uncaughtExceptionHandler.uncaughtException(thread, throwable);
+      }
+      System.exit(1);
+    } catch (Throwable th) {
+      th.printStackTrace();
+    }
   }
 }

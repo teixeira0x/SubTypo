fe60ced40a4236a6f898cd8aefa45dfd418a82e4
teixeira0x <secretedit123e@gmail.com>
2024-08-08 22:40:34 -0300

    fix: update version code to 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     applicationId = "com.teixeira.subtitles"
     minSdk = 24
     targetSdk = 34
-    versionCode = 1
+    versionCode = 2
     versionName = "1.1.0"
 
     vectorDrawables { useSupportLibrary = true }

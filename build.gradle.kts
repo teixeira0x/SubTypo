627961bccd060a41507e1df6f2e518acff5eed2c
teixeira0x <secretedit123e@gmail.com>
2024-08-29 20:16:26 -0300

    update version to 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ fun Project.configureBaseExtension() {
     defaultConfig {
       minSdk = 24
       targetSdk = 34
-      versionCode = 2
-      versionName = "1.1.1"
+      versionCode = 3
+      versionName = "1.2.0"
     }
 
     compileOptions {

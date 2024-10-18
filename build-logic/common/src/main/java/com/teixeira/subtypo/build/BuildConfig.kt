ac7121e2f78a4b4f21d349fe652c93a9b696a3d1
Felipe Teixeira <secretedit123e@gmail.com>
2024-10-17 22:54:17 -0300

    fix(build-logic): add common module src

---
+++ build-logic/common/src/main/java/com/teixeira/subtypo/build/BuildConfig.kt
@@ -0,0 +1,11 @@
+package com.teixeira.subtypo.build
+
+@SuppressWarnings("unused")
+object BuildConfig {
+  const val packageName = "com.teixeira.subtitles"
+
+  const val compileSdk = 34
+  const val buildTools = "34.0.0"
+  const val minSdk = 24
+  const val targetSdk = 34
+}

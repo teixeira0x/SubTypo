f52ce3f6c33cc9717fb18ea0af2dcb7b5fe2098e
Felipe Teixeira <secretedit123e@gmail.com>
2024-12-06 07:22:06 -0300

    fix(build-logic): add missing module

---
+++ build-logic/common/src/main/java/com/teixeira0x/subtypo/build/BuildConfig.kt
@@ -0,0 +1,11 @@
+package com.teixeira0x.subtypo.build
+
+@SuppressWarnings("unused")
+object BuildConfig {
+  const val packageName = "com.teixeira0x.subtypo"
+
+  const val compileSdk = 34
+  const val buildTools = "34.0.0"
+  const val minSdk = 24
+  const val targetSdk = 34
+}
---
+++ build-logic/common/src/main/java/com/teixeira0x/subtypo/build/VersionUtils.kt
@@ -0,0 +1,28 @@
+/*
+ * This file is part of SubTypo.
+ *
+ * SubTypo is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * SubTypo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with SubTypo.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.teixeira0x.subtypo.build
+
+@SuppressWarnings("unused")
+object VersionUtils {
+  const val versionCode = 3
+  const val version = "1.2.0"
+
+  val versionName by lazy {
+    if (CI.isCiBuild && CI.branchName != "main") {
+      "$version-${CI.commitHash}-${CI.branchName}"
+    } else version
+  }
+}
